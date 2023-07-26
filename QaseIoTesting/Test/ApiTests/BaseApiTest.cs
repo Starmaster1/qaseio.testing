c85218180d1500d6498f45ff6ea7787def515f5b
Alexander Starostin <starostin.g@gmail.com>
2023-07-26 22:14:57 +0500

    Для api тестов включил логи allure

--- QaseIoTesting/Test/ApiTests/BaseApiTest.cs
+++ QaseIoTesting/Test/ApiTests/BaseApiTest.cs
@@ -1,9 +1,12 @@
 ﻿using Core.API;
 using Core.Configuration;
 using Microsoft.Extensions.Configuration;
+using NUnit.Allure.Core;
 
 namespace Test.ApiTests
 {
+    [TestFixture]
+    [AllureNUnit]
     public class BaseApiTest
     {
         protected BaseApiClient apiClient;
